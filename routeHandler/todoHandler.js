c71820970500cd36ba5cf608230e08d948905ff3
learnwithsumit <play@learnwithsumit.com>
2021-03-28 22:05:57 +0600

    source code upload

---
+++ routeHandler/todoHandler.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const todoSchema = require("../schemas/todoSchema");
+const Todo = new mongoose.model("Todo", todoSchema);
+
+// GET ALL THE TODOS
+router.get("/", async (req, res) => {
+  await Todo.find({ status: "active" })
+    .select({
+      _id: 0,
+      _v: 0,
+      date: 0,
+    })
+    .limit(2)
+    .exec((err, data) => {
+      if (err) {
+        res.status(500).json({
+          error: "There was a server side error!",
+        });
+      } else {
+        res.status(200).json({
+          result: data,
+          message: "Success",
+        });
+      }
+    });
+});
+
+// GET A TODO by ID
+router.get("/:id", async (req, res) => {
+  await Todo.find({ _id: req.params.id }, (err, data) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was a server side error!",
+      });
+    } else {
+      res.status(200).json({
+        result: data,
+        message: "Success",
+      });
+    }
+  });
+});
+
+// POST A TODO
+router.post("/", async (req, res) => {
+  const newTodo = new Todo(req.body);
+  await newTodo.save((err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was a server side error!",
+      });
+    } else {
+      res.status(200).json({
+        message: "Todo was inserted successfully!",
+      });
+    }
+  });
+});
+
+// POST MULTIPLE TODO
+router.post("/all", async (req, res) => {
+  await Todo.insertMany(req.body, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was a server side error!",
+      });
+    } else {
+      res.status(200).json({
+        message: "Todos were inserted successfully!",
+      });
+    }
+  });
+});
+
+// PUT TODO
+router.put("/:id", async (req, res) => {
+  const result = await Todo.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: {
+        status: "active",
+      },
+    },
+    {
+      new: true,
+      useFindAndModify: false,
+    },
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          error: "There was a server side error!",
+        });
+      } else {
+        res.status(200).json({
+          message: "Todo was updated successfully!",
+        });
+      }
+    }
+  );
+  console.log(result);
+});
+
+// DELETE TODO
+router.delete("/:id", async (req, res) => {
+  await Todo.deleteOne({ _id: req.params.id }, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was a server side error!",
+      });
+    } else {
+      res.status(200).json({
+        message: "Todo was deleted successfully!",
+      });
+    }
+  });
+});
+
+module.exports = router;
---
+++ schemas/todoSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const todoSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = todoSchema;
